c81c76e18027e39c0c5e85264515016186e95284
Cyraullie <cyril.goldenschue@gmail.com>
2024-11-14 11:22:30 +0100

    add all function

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:56:21 by cgoldens          #+#    #+#             */
-/*   Updated: 2024/11/12 15:58:54 by cgoldens         ###   ########.fr       */
+/*   Updated: 2024/11/13 14:28:10 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 # define PUSH_SWAP_H
 
 # include "libft/libft.h"
+
+void	check_pile(t_list *list);
+void	free_list(t_list *a);
+
+void	sa(t_list **a);
+void	sb(t_list **b);
+void	ss(t_list **a, t_list **b);
+
+void	pa(t_list **a, t_list **b);
+void	pb(t_list **a, t_list **b);
+
+void	ra(t_list **a);
+void	rb(t_list **b);
+void	rr(t_list **a, t_list **b);
+
+void	rra(t_list **a);
+void	rrb(t_list **b);
+void	rrr(t_list **a, t_list **b);
 
 #endif
