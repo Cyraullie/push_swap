5e3a6991b839a10214ff26a7f7c732d6032597e2
Cyraullie <cyril.goldenschue@gmail.com>
2024-11-15 15:07:26 +0100

    try do sort5

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 13:06:53 by cgoldens          #+#    #+#             */
-/*   Updated: 2024/11/14 17:09:01 by cgoldens         ###   ########.fr       */
+/*   Updated: 2024/11/15 14:04:27 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,18 +46,18 @@ void	sort_five(t_pile *pile)
 	int	len;
 
 	len = ft_lstsize(pile->pile_a);
-	while (len--)
+	/*while (len--)
 	{
 		if (pile->pile_a->index == 0 || pile->pile_a->index == 1)
 			handle_format(pile, "pb");
 		else
 			handle_format(pile, "ra");
-	}
-	sort_3nbr(pile);
+	}*/
+	/*sort_three(pile);
 	handle_format(pile, "pa");
 	handle_format(pile, "pa");
 	if (pile->pile_a->content > pile->pile_a->next->content)
-		handle_format(pile, "sa");
+		handle_format(pile, "sa");*/
 }
 
 void	check_pile(t_list *list)
@@ -67,7 +67,15 @@ void	check_pile(t_list *list)
 	tmp = list;
 	while (tmp != NULL)
 	{
-		printf("%d\n", *(int *)tmp->content);
+		printf("%d _index:%d\n", *(int *)tmp->content, tmp->index);
 		tmp = tmp->next;
 	}
+}
+
+void	debug_piles(t_pile *pile)
+{
+	printf("\na\n-\n");
+	check_pile(pile->pile_a);
+	printf("\nb\n-\n");
+	check_pile(pile->pile_b);
 }
