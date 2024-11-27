951594b3ff2a37291b81dc1d63cb69c70146123d
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2024-11-27 14:18:58 +0100

    norm push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:56:21 by cgoldens          #+#    #+#             */
-/*   Updated: 2024/11/26 16:30:52 by cgoldens         ###   ########.fr       */
+/*   Updated: 2024/11/27 14:18:05 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,16 @@ typedef struct s_pile
 	t_list	*pile_b;
 }	t_pile;
 
+void	free_args(char **args);
+void	turk_sort(int size, int min, t_pile *piles);
+void	turk_loop(int size, int min, int max_bits, t_pile *piles);
+int		free_all(int argc, char **args, t_pile *pile);
+
 int		check_double(t_list *a);
 int		is_number(const char *str);
 int		is_int(const char *str);
 int		is_sorted(t_list *a);
-int		check_errors(int argc, char **argv);
+int		check_errors(int argc, char **argv, t_pile *pile);
 
 void	debug_piles(t_pile *pile);
 void	add_index(t_list *lst);
@@ -39,7 +44,7 @@ void	sort_five(t_pile *pile);
 int		isrevsorted(t_pile	*pile);
 int		find_position(t_list *list, int target);
 
-void    move_top_util(int position, t_pile *pile, char stack_name, char *base);
+void	move_top_util(int position, t_pile *pile, char stack_name, char *base);
 void	move_min_to_b(t_pile *pile);
 void	move_to_top(t_pile *pile, int value, char stack_name);
 int		find_min(t_list *pile);
@@ -59,24 +64,11 @@ void	rra(t_list **a);
 void	rrb(t_list **b);
 void	rrr(t_list **a, t_list **b);
 
-void turk_sort(t_pile *piles);
-int find_min_value(t_list *pile);
-int pile_size(t_list *pile);
-/*void radix_sort(t_pile *piles);
-int find_min_index(t_list *pile);*/
-/*
-void quick_sort_push_swap(t_pile *piles);
-//void quick_sort_a(t_pile *piles, int len, int count_r);
-//void quick_sort_b(t_pile *piles, int len, int count_r);
-//int find_median(t_list *pile, int len);
-void sort_small_a(t_pile *piles, int len);
-void sort_small_b(t_pile *piles, int len);
-int is_sorted(t_list *pile, int len, int ascending);
-int find_min_position(t_list *pile);
-void quick_sort_recursive(t_pile *piles, int is_a);
-void merge_piles(t_pile *piles);
-void merge_into_a(t_pile *piles, int size_b);
-void merge_into_b(t_pile *piles, int size_a);
-void sort_small(t_pile *piles, int size);
-*/
+int		pile_size(t_list *pile);
+int		find_max(t_list *pile);
+int		find_min(t_list *pile);
+void	turkish_sort(t_pile *piles);
+void	find_min_max(t_list *pile, int *min, int *max);
+int		get_max_bits(int max_num);
+
 #endif
