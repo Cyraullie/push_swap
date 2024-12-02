a05f379fb7a78355a188212493f3de17b118d4e9
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2024-12-02 16:33:22 +0100

    rename file

---
+++ free.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 16:46:10 by cgoldens          #+#    #+#             */
+/*   Updated: 2024/12/02 16:29:47 by cgoldens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	free_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
+
+void	free_list(t_list **lst)
+{
+	t_list	*tmp;
+
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		free((*lst)->content);
+		free(*lst);
+		*lst = tmp;
+	}
+	*lst = NULL;
+}
+
+int	free_all(int argc, char **args, t_pile *pile)
+{
+	if (argc == 2)
+		free_args(args);
+	free_list(&(pile->pile_a));
+	free_list(&(pile->pile_b));
+	free(pile);
+	return (0);
+}
